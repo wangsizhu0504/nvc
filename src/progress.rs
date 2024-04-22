5d3baaca41e6026610dbd68f860cef31f5b471a2
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2024-04-22 10:47:27 +0800

    style: progress style

--- src/progress.rs
+++ src/progress.rs
@@ -4,19 +4,36 @@ use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 
 use reqwest::blocking::Response;
 
+const PROGRESS_CHARS: &str = if cfg!(windows) {
+    "█▓▒░  "
+} else {
+    "█▉▊▋▌▍▎▏  "
+};
+
+const INDICATIF_PROGRESS_TEMPLATE: &str = if cfg!(windows) {
+    // Do not use a spinner on Windows since the default console cannot display
+    // the characters used for the spinner
+    "{elapsed_precise:.bold} ▐{wide_bar:.blue/white.dim}▌ {bytes}/{total_bytes} {percent:.bold}% ({eta})"
+} else {
+    "{spinner:.green.bold} [{elapsed_precise:.bold}] ▕{wide_bar:.blue/white.dim}▏ {bytes}/{total_bytes} {percent:.bold}%  ({eta})"
+};
+
+
 pub struct ResponseProgress {
     progress: Option<ProgressBar>,
     response: Response,
 }
 
 fn make_progress_bar(size: u64) -> ProgressBar {
-
     let pb = ProgressBar::new(size);
-    pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
-        .unwrap()
-        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
-        .progress_chars("#>-"));
-
+    pb.set_style(
+        ProgressStyle::default_bar()
+            .template(INDICATIF_PROGRESS_TEMPLATE)
+            .unwrap()
+            .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
+            .progress_chars(PROGRESS_CHARS),
+    );
+    
     pb
 }
 
@@ -29,7 +46,7 @@ impl ResponseProgress {
             response,
         }
     }
-
+    
     pub fn finish(&self) {
         if let Some(ref bar) = self.progress {
             bar.finish_with_message("downloaded");
@@ -40,11 +57,11 @@ impl ResponseProgress {
 impl Read for ResponseProgress {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         let size = self.response.read(buf)?;
-
+        
         if let Some(ref bar) = self.progress {
             bar.inc(size as u64);
         }
-
+        
         Ok(size)
     }
 }
